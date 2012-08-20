8341381d057e42afafdcc23c88bee32f7fb71d9e
Mauricio Carneiro <carneiro@broadinstitute.org>
2012-08-20 11:33:22 -0400

    Quick separation of math and quality utils to mimic the GATK infrastructure

--- MathUtils.java
+++ MathUtils.java
@@ -4,69 +4,19 @@
  */
 public class MathUtils {
 
-    private static final int MAXN = 50000;
-    private static double[] qualToErrorProbLog10Cache = new double[256];
-    private static double[] qualToProbLog10Cache = new double[256];
-    public static final double[] log10Cache;
-    public static final double[] log10FactorialCache;
     private static final double[] jacobianLogTable;
     private static final double JACOBIAN_LOG_TABLE_STEP = 0.001;
     private static final double MAX_JACOBIAN_TOLERANCE = 8.0;
     private static final double JACOBIAN_LOG_TABLE_INV_STEP = 1.0 / 0.001;
     private static final int JACOBIAN_LOG_TABLE_SIZE = (int) (MAX_JACOBIAN_TOLERANCE / JACOBIAN_LOG_TABLE_STEP) + 1;
-    private static final int LOG10_CACHE_SIZE = 4 * MAXN;  // we need to be able to go up to 2*(2N) when calculating some of the coefficients
 
     static {
-        for (int i = 0; i < 256; i++)
-            qualToErrorProbLog10Cache[i] = MathUtils.qualToErrorProbLog10Raw(i);
-        for (int i = 0; i < 256; i++)
-            qualToProbLog10Cache[i] = MathUtils.qualToProbLog10Raw(i);
-
-        log10Cache = new double[LOG10_CACHE_SIZE];
-        log10FactorialCache = new double[LOG10_CACHE_SIZE];
         jacobianLogTable = new double[JACOBIAN_LOG_TABLE_SIZE];
-
-        log10Cache[0] = Double.NEGATIVE_INFINITY;
-        for (int k = 1; k < LOG10_CACHE_SIZE; k++) {
-            log10Cache[k] = Math.log10(k);
-            log10FactorialCache[k] = log10FactorialCache[k - 1] + log10Cache[k];
-        }
 
         for (int k = 0; k < JACOBIAN_LOG_TABLE_SIZE; k++) {
             jacobianLogTable[k] = Math.log10(1.0 + Math.pow(10.0, -((double) k) * JACOBIAN_LOG_TABLE_STEP));
 
         }
-    }
-
-    static private double qualToProbLog10Raw(int qual) {
-        return Math.log10(1.0 - qualToErrorProbRaw(qual));
-    }
-
-    static public double qualToProbLog10(byte qual) {
-        return qualToProbLog10Cache[(int) qual & 0xff]; // Map: 127 -> 127; -128 -> 128; -1 -> 255; etc.
-    }
-
-    /**
-     * Convert a quality score to a probability of error.  This is the Phred-style conversion, *not* the Illumina-style
-     * conversion (though asymptotically, they're the same).
-     *
-     * @param qual a quality score (0 - 255)
-     * @return a probability (0.0 - 1.0)
-     */
-    static private double qualToErrorProbRaw(int qual) {
-        return qualToErrorProb((double) qual);
-    }
-
-    public static double qualToErrorProb(final double qual) {
-        return Math.pow(10.0, (qual) / -10.0);
-    }
-
-    static private double qualToErrorProbLog10Raw(int qual) {
-        return ((double) qual) / -10.0;
-    }
-
-    static public double qualToErrorProbLog10(byte qual) {
-        return qualToErrorProbLog10Cache[(int) qual & 0xff]; // Map: 127 -> 127; -128 -> 128; -1 -> 255; etc.
     }
 
     // A fast implementation of the Math.round() method.  This method does not perform
