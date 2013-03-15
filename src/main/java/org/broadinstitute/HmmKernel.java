62970d347e1e8e640224c97e4bccdf76f5fc4b92
Phil Pratt-Szeliga <pcpratts@trifort.org>
2013-03-15 18:55:48 -0400

    HmmKernel now calls StandardPairHMM.

--- src/main/java/org/broadinstitute/HmmKernel.java
+++ src/main/java/org/broadinstitute/HmmKernel.java
@@ -1,6 +1,8 @@
 package org.broadinstitute;
 
 import edu.syr.pcpratts.rootbeer.runtime.Kernel;
+import org.broadinstitute.pairhmm.StandardPairHMM;
+import org.broadinstitute.utils.QualityUtils;
 
 public class HmmKernel implements Kernel {
 
@@ -14,6 +16,9 @@ public class HmmKernel implements Kernel {
     private byte[] overallGCP;
     private int hapStartIndex;
     private boolean recacheReadValues;
+
+    private static final int X_METRIC_LENGTH = 10000;
+    private static final int Y_METRIC_LENGTH = 10000;
 
     public HmmKernel(final byte[] haplotypeBases, final byte[] readBases, 
       final byte[] readQuals, final byte[] insertionGOP, 
@@ -32,10 +37,28 @@ public class HmmKernel implements Kernel {
     }
 
     public void gpuMethod(){
-      likelihood = 0;
+      StandardPairHMM hmm = new StandardPairHMM();
+      hmm.initialize(X_METRIC_LENGTH + 2, Y_METRIC_LENGTH + 2);
+      likelihood = hmm.subComputeReadLikelihoodGivenHaplotypeLog10(haplotypeBases, 
+        readBases, cleanupQualityScores(readQuals), insertionGOP, deletionGOP, 
+        overallGCP, hapStartIndex, recacheReadValues);
     }
   
     public double getLikelihood(){
       return likelihood;
+    }
+
+
+    /**
+     * Ensures that all the qual scores have valid values
+     *
+     * @param readQuals read qualities array (modified in place)
+     * @return the readQuals array for convenience
+     */
+    private byte[] cleanupQualityScores(byte[] readQuals) {
+        for (int i = 0; i < readQuals.length; i++) {
+            readQuals[i] = (readQuals[i] < QualityUtils.MIN_USABLE_Q_SCORE ? QualityUtils.MIN_USABLE_Q_SCORE : (readQuals[i] > Byte.MAX_VALUE ? Byte.MAX_VALUE : readQuals[i]));
+        }
+        return readQuals;
     }
 }
