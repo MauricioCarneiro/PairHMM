55f631a51c2126801513de2c7d7fad6fb0a125d3
Mauricio Carneiro <carneiro@broadinstitute.org>
2014-03-29 21:00:02 -0400

    Fix recalculate function lack of reserve for the constant and diagonal
    * add memory reserve to recalculate
    * add project_code dependency to make check
    * remove m_padded_haplotypes
    * make max_padded_read_lenght local
    * make max_original_read_length reference clearer
    * avoid checking all the reads a second time for padded_reads max length calculation
    * add other to .gitignore for extra testdata

--- src/pairhmm.h
+++ src/pairhmm.h
@@ -13,8 +13,8 @@ class Pairhmm {
   CORRECT correct;
  public:
   std::vector<double> calculate(const Testcase& testcase) {
-    auto results = fast.calculate(testcase);  // calculate all the testcases using the float precision pairhmm implementation
-    correct.recalculate(testcase, results, fast.max_original_read_length(), fast.max_padded_read_length()); // recalculate only the ones that failed with double precision (results = DOUBLE_MIN for failed ones)
+    auto results = fast.calculate(testcase); // calculate all the testcases using the float precision pairhmm implementation
+    correct.recalculate(testcase, results);  // recalculate only the ones that failed with double precision (results = DOUBLE_MIN for failed ones)
     return results;
   }
 };
